dbe0a367c98a0a6f7e3180a007cb87f463e3767e
SIDDHARThMNC <siddhucaa@gmail.com>
2025-09-24 02:39:58 +0530

    first commit

---
+++ shopping-app/src/pages/Cart.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux';
+import CartItem from '../components/CartItem';
+import { Link } from "react-router-dom";
+
+const Cart = () => {
+  const { cart } = useSelector((state) => state);
+  console.log("Printing Cart");
+  console.log(cart);
+  const [totalAmout, setTotalAmount] = useState(0);
+
+  useEffect(() => {
+    setTotalAmount(cart.reduce((acc, curr) => acc + curr.price, 0));
+  }, [cart])
+
+  return (
+    <div>
+      {
+        cart.length > 0 ?
+          (<div>
+
+            <div>
+              {
+                cart.map((item, index) => {
+                  return <CartItem key={item.id} item={item} itemIndex={index} />
+                })
+              }
+            </div>
+
+            <div>
+
+              <div>
+                <div>Your Cart</div>
+                <div>Summary</div>
+                <p>
+                  <span>Total Items:{cart.length}</span>
+                </p>
+              </div>
+
+              <div>
+                <p>Total Amount: ${totalAmout}</p>
+                <button>Checkout Now</button>
+
+              </div>
+            </div>
+
+          </div>) :
+          (<div>
+            <h1>Cart Empty </h1>
+            <Link to={"/"}>
+              <button>
+                Shop now
+              </button>
+            </Link>
+          </div>)
+      }
+    </div>
+  )
+}
+
+export default Cart;
